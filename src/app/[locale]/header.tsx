a5e2611a9424c62b0d6372241ad0509d905e0797
Jan Duras <durasjan1@gmail.com>
2025-01-05 22:06:36 +0100

    Set header bg

--- src/app/[locale]/header.tsx
+++ src/app/[locale]/header.tsx
@@ -33,7 +33,7 @@ const Header = async () => {
       align="center"
       gap="3"
       p="4"
-      className="bg-panel-solid z-10"
+      className="bg-gray-1 z-10"
       asChild
     >
       <header>
