c05d3b0dbbf2297fc7bca3cb956e0d4cdfe27d60
Jan Duras <durasjan1@gmail.com>
2025-01-07 20:37:38 +0100

    Fix skeleton for file upload

--- src/app/[locale]/order/item/[id]/files/skeleton.tsx
+++ src/app/[locale]/order/item/[id]/files/skeleton.tsx
@@ -1,21 +1,48 @@
-import { Grid, Skeleton } from "@radix-ui/themes";
+import { Flex, Text, Grid, Skeleton } from "@radix-ui/themes";
 
 const FilesSkeleton = () => {
   return (
-    <Grid
-      columns={{
-        initial: "2",
-        xs: "3",
-        sm: "4",
-        md: "5",
-        lg: "6",
-        xl: "7",
-      }}
-      gap="3"
-      width="full"
-    >
-      <Skeleton className="rounded-3 h-[8rem] cursor-wait" />
-    </Grid>
+    <Flex direction="column" gap="4">
+      <Text
+        className="flex items-center justify-center border border-dashed border-gray-8 p-6 rounded-3 text-gray-11 cursor-pointer hover:bg-gray-3"
+        as="label"
+      >
+        <Flex
+          direction="column"
+          align="center"
+          gap="5"
+          className="max-w-sm text-center"
+          my="2"
+        >
+          <Flex direction="column" align="center" gap="3">
+            <Skeleton className="rounded-full w-[40] h-[40] cursor-wait" />
+
+            <Skeleton className="rounded-3 w-[352] h-[24] cursor-wait" />
+          </Flex>
+
+          <Flex direction="column" align="center" gap="1">
+            <Text color="gray" size="1" className="flex items-center gap-1.5">
+              <Skeleton className="rounded-3 w-[220] h-[16] cursor-wait" />
+            </Text>
+            <Text color="gray" size="1">
+              <Skeleton className="rounded-3 w-[312] h-[16] cursor-wait" />
+            </Text>
+          </Flex>
+        </Flex>
+      </Text>
+
+      <Grid
+        columns={{
+          initial: "2",
+          xs: "3",
+          sm: "5",
+          md: "3",
+          lg: "4",
+        }}
+        gap="4"
+        width="full"
+      ></Grid>
+    </Flex>
   );
 };
 
