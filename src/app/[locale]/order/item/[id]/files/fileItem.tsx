95304cba5b154a0bb7eabf0810aef82334145ce5
Jan Duras <durasjan1@gmail.com>
2025-02-22 13:19:59 +0100

    Add sample dialog on file item open

--- src/app/[locale]/order/item/[id]/files/fileItem.tsx
+++ src/app/[locale]/order/item/[id]/files/fileItem.tsx
@@ -10,6 +10,8 @@ import {
   Spinner,
   Text,
   Tooltip,
+  Dialog,
+  TextField,
 } from "@radix-ui/themes";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
@@ -52,59 +54,103 @@ const FileItem = ({
   return (
     <Flex direction="column" className="ring-1 ring-gray-6 rounded-3">
       <Box className="bg-gray-2 rounded-t-3 h-32 relative text-left p-0 overflow-hidden">
-        <Tooltip content={isUploading ? t("uploading") : t("open")}>
-          <Button
-            color="gray"
-            disabled={isUploading}
-            className="relative size-full text-accent-contrast p-0 bg-gray-3 hover:opacity-60 transition-all"
-          >
-            {!isUploading && !hasThumbnail && (
-              <Text
+        <Dialog.Root>
+          <Tooltip content={isUploading ? t("uploading") : t("open")}>
+            <Dialog.Trigger>
+              <Button
                 color="gray"
-                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl mt-1"
+                disabled={isUploading}
+                className="relative size-full text-accent-contrast p-0 bg-gray-3 hover:opacity-60 transition-all"
               >
-                {extension}
-              </Text>
-            )}
+                {!isUploading && !hasThumbnail && (
+                  <Text
+                    color="gray"
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl mt-1"
+                  >
+                    {extension}
+                  </Text>
+                )}
 
-            {hasThumbnail && (
-              <Image
-                unoptimized
-                src={`/api/s3/thumbnail/${s3Key}`}
-                alt=""
-                width={256}
-                height={256}
-                className="object-cover size-full"
-                onError={(event) =>
-                  ((event.target as HTMLImageElement).style.display = "none")
-                }
-              />
-            )}
-            {isUploading && (
-              <>
-                <Progress
-                  value={progressPercentage || 0}
-                  color="blue"
-                  variant="soft"
-                  radius="none"
-                  className="absolute top-0 left-0 size-full bg-blue-5"
+                {hasThumbnail && (
+                  <Image
+                    unoptimized
+                    src={`/api/s3/thumbnail/${s3Key}`}
+                    alt=""
+                    width={256}
+                    height={256}
+                    className="object-cover size-full"
+                    onError={(event) =>
+                      ((event.target as HTMLImageElement).style.display =
+                        "none")
+                    }
+                  />
+                )}
+                {isUploading && (
+                  <>
+                    <Progress
+                      value={progressPercentage || 0}
+                      color="blue"
+                      variant="soft"
+                      radius="none"
+                      className="absolute top-0 left-0 size-full bg-blue-5"
+                    />
+                    <Box
+                      position="absolute"
+                      top="50%"
+                      left="50%"
+                      className="transform -translate-x-1/2 -translate-y-1/2 text-3xl mt-1"
+                    >
+                      {progressPercentage === 100 ? (
+                        <Spinner size="3" />
+                      ) : (
+                        (progressPercentage || 0) + "%"
+                      )}
+                    </Box>
+                  </>
+                )}
+              </Button>
+            </Dialog.Trigger>
+          </Tooltip>
+
+          <Dialog.Content maxWidth="450px">
+            <Dialog.Title>Edit profile</Dialog.Title>
+            <Dialog.Description size="2" mb="4">
+              Make changes to your profile.
+            </Dialog.Description>
+
+            <Flex direction="column" gap="3">
+              <label>
+                <Text as="div" size="2" mb="1" weight="bold">
+                  Name
+                </Text>
+                <TextField.Root
+                  defaultValue="Freja Johnsen"
+                  placeholder="Enter your full name"
                 />
-                <Box
-                  position="absolute"
-                  top="50%"
-                  left="50%"
-                  className="transform -translate-x-1/2 -translate-y-1/2 text-3xl mt-1"
-                >
-                  {progressPercentage === 100 ? (
-                    <Spinner size="3" />
-                  ) : (
-                    (progressPercentage || 0) + "%"
-                  )}
-                </Box>
-              </>
-            )}
-          </Button>
-        </Tooltip>
+              </label>
+              <label>
+                <Text as="div" size="2" mb="1" weight="bold">
+                  Email
+                </Text>
+                <TextField.Root
+                  defaultValue="freja@example.com"
+                  placeholder="Enter your email"
+                />
+              </label>
+            </Flex>
+
+            <Flex gap="3" mt="4" justify="end">
+              <Dialog.Close>
+                <Button variant="soft" color="gray">
+                  Cancel
+                </Button>
+              </Dialog.Close>
+              <Dialog.Close>
+                <Button>Save</Button>
+              </Dialog.Close>
+            </Flex>
+          </Dialog.Content>
+        </Dialog.Root>
 
         <Tooltip content={t("removeFile")}>
           <IconButton
